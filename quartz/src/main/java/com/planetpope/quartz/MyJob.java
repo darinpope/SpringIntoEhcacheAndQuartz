f7b23c15352b8ad7083bfd5f2287155dc6a4f735
Darin Pope <darin@planetpope.com>
2011-06-08 10:39:15 -0700

    clean up job definition

--- quartz/src/main/java/com/planetpope/quartz/MyJob.java
+++ quartz/src/main/java/com/planetpope/quartz/MyJob.java
@@ -2,13 +2,11 @@ package com.planetpope.quartz;
 
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
-import org.quartz.StatefulJob;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
-public class MyJob extends QuartzJobBean implements StatefulJob {
+public class MyJob extends QuartzJobBean {
 
     protected void executeInternal(JobExecutionContext ctx) throws JobExecutionException {
-        // place your Job code here
         System.out.println("Executing Quartz Job");
     }
 }
